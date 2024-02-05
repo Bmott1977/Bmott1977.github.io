7db048a2fe240c5defab696c1c0df4ecb3714a69
Brendan Mott <500172880@fvtc.edu>
2024-02-05 00:00:34 -0600

    Added welcome div

---
+++ site.js
@@ -0,0 +1,18 @@
+//Gets hours of the day
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+//Changes message based on time of day
+if ( isMorning == true) {
+    document.querySelector('#Welcome p').textContent = "Welcome to my Hompage, it's currently the morning."
+
+} else if (isAfternoon == true) {
+    document.querySelector('#Welcome p').textContent = "Welcome to my Hompage, it's currently the afternoon."
+
+} else if (isEvening == true) {
+    document.querySelector('#Welcome p').textContent = "Welcome to my Hompage, it's currently the evening."
+
+}
