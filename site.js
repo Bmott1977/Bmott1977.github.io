70116ac9ec1db2e88a50edb39e438b1756b6c082
Brendan Mott <500172880@fvtc.edu>
2024-02-25 16:18:04 -0600

    Added secret message

--- site.js
+++ site.js
@@ -6,13 +6,15 @@ const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
 
 //Changes message based on time of day
-if ( isMorning == true) {
+if ( isMorning === true) {
     document.querySelector('#Welcome p').textContent = "Welcome to my Hompage, it's currently the morning."
 
-} else if (isAfternoon == true) {
+} else if (isAfternoon === true) {
     document.querySelector('#Welcome p').textContent = "Welcome to my Hompage, it's currently the afternoon."
 
-} else if (isEvening == true) {
+} else if (isEvening === true) {
     document.querySelector('#Welcome p').textContent = "Welcome to my Hompage, it's currently the evening."
 
 }
+
+localStorage.setItem('Its a secret to everybody.', '100 Rupees')
